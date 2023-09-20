fbf4ddf271c16c59d438984658dda6a71434cf6f
Mmmansta <128576312+Mmmansta@users.noreply.github.com>
2023-09-20 11:13:51 +0300

    pp1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,24 +6,33 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Jeck","Sparrow", (byte) 35);
         userService.saveUser("Hector", "Barbosa", (byte) 55);
         userService.saveUser("William", "Turner ||", (byte) 27);
         userService.saveUser("Tia", "Dalma", (byte) 99);
-        for (User allUser : userService.getAllUsers()) {
-            System.out.println(allUser);
+        try {
+            for (User allUser : userService.getAllUsers()) {
+                System.out.println(allUser);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
+
 
 
     }
